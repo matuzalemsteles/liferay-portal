c5418e750ec729239090300901429868c45fc643
Patrick Yeo <patrick.yeo@liferay.com>
2023-08-04 10:36:53 -0700

    LPS-184404 Control Menu should expand if fluid layout is enabled

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/ContainerTag.java
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/ContainerTag.java
@@ -56,7 +56,7 @@ public class ContainerTag extends BaseContainerTag {
 		else {
 			cssClasses.add("container-fluid");
 
-			if (Validator.isNotNull(_size)) {
+			if (Validator.isNotNull(_size) && !(_size == "false")) {
 				cssClasses.add("container-fluid-max-" + _size);
 			}
 		}
